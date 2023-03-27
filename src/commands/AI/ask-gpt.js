e08a9196362fddc2c79cdbcac4b45bd3582a42bb
Imabanana80 <imabanana80.hallo@gmail.com>
2023-03-27 12:28:36 +0800

    disabled ask-gpt

--- src/commands/AI/ask-gpt.js
+++ src/commands/AI/ask-gpt.js
@@ -8,26 +8,27 @@ module.exports = class askgptSlashCommand extends baseSlashCommand {
   }
   async run(client, interaction) {
     require("dotenv").config();
-    await interaction.deferReply();
-    const prompt = interaction.options.get("prompt").value;
-    const openaiConfig = new Configuration({
-      apiKey: process.env.OPENAIKEY,
-    });
-    const openai = new OpenAIApi(openaiConfig);
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 1024,
-    });
-    const embed = new EmbedBuilder()
-      .setDescription(`\`\`\`${response.data.choices[0].text}\`\`\``)
-      .setTitle(`Prompt: ${prompt} `)
-      .setFooter({
-        text: `Prompt: ${response.data.usage.prompt_tokens} | Response: ${response.data.usage.completion_tokens} | Total: ${response.data.usage.total_tokens}`,
-      })
-      .setColor(0xfaff86);
-    interaction.editReply({ embeds: [embed] });
+    // await interaction.deferReply();
+    // const prompt = interaction.options.get("prompt").value;
+    // const openaiConfig = new Configuration({
+    //   apiKey: process.env.OPENAIKEY,
+    // });
+    // const openai = new OpenAIApi(openaiConfig);
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: `${prompt}`,
+    //   temperature: 0,
+    //   max_tokens: 1024,
+    // });
+    // const embed = new EmbedBuilder()
+    //   .setDescription(`\`\`\`${response.data.choices[0].text}\`\`\``)
+    //   .setTitle(`Prompt: ${prompt} `)
+    //   .setFooter({
+    //     text: `Prompt: ${response.data.usage.prompt_tokens} | Response: ${response.data.usage.completion_tokens} | Total: ${response.data.usage.total_tokens}`,
+    //   })
+    //   .setColor(0xfaff86);
+    // interaction.editReply({ embeds: [embed] });
+    interactiom.editReply("This command has been temporarily disabled");
   }
   getSlashCommandJSON() {
     return new SlashCommandBuilder()
