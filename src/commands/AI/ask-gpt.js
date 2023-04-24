a56282de9b6048f9c953d0b0c67f2d8ffc000f60
Imabanana80 <imabanana80.hallo@gmail.com>
2023-04-24 15:34:40 +0800

    'enabled' ask-gpt and made it require BOT OWNER perms

--- src/commands/AI/ask-gpt.js
+++ src/commands/AI/ask-gpt.js
@@ -1,39 +1,37 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const baseSlashCommand = require("../../utils/baseSlashCommand");
 const { Configuration, OpenAIApi } = require("openai");
+const { noPerms } = require("../../utils/functions");
 
 module.exports = class askgptSlashCommand extends baseSlashCommand {
   constructor() {
     super("ask-gpt");
   }
   async run(client, interaction) {
-    interaction.deferReply();
-    interaction.editReply({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("This command has been disabled till further notice.")
-          .setColor("Red"),
-      ],
-    });
-    // const prompt = interaction.options.get("prompt").value;
-    // const openaiConfig = new Configuration({
-    //   apiKey: process.env.OPENAIKEY,
-    // });
-    // const openai = new OpenAIApi(openaiConfig);
-    // const response = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: `${prompt}`,
-    //   temperature: 0,
-    //   max_tokens: 1024,
-    // });
-    // const embed = new EmbedBuilder()
-    //   .setDescription(`\`\`\`${response.data.choices[0].text}\`\`\``)
-    //   .setTitle(`Prompt: ${prompt} `)
-    //   .setFooter({
-    //     text: `Prompt: ${response.data.usage.prompt_tokens} | Response: ${response.data.usage.completion_tokens} | Total: ${response.data.usage.total_tokens}`,
-    //   })
-    //   .setColor(0xfaff86);
-    // interaction.editReply({ embeds: [embed] });
+    await interaction.deferReply();
+    if (interaction.user.id != process.env.OWNERID) {
+      await noPerms(interaction, "BOT OWNER");
+    } else {
+      const prompt = interaction.options.get("prompt").value;
+      const openaiConfig = new Configuration({
+        apiKey: process.env.OPENAIKEY,
+      });
+      const openai = new OpenAIApi(openaiConfig);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 1024,
+      });
+      const embed = new EmbedBuilder()
+        .setDescription(`\`\`\`${response.data.choices[0].text}\`\`\``)
+        .setTitle(`Prompt: ${prompt} `)
+        .setFooter({
+          text: `Prompt: ${response.data.usage.prompt_tokens} | Response: ${response.data.usage.completion_tokens} | Total: ${response.data.usage.total_tokens}`,
+        })
+        .setColor(0xfaff86);
+      interaction.editReply({ embeds: [embed] });
+    }
   }
   getSlashCommandJSON() {
     return new SlashCommandBuilder()
