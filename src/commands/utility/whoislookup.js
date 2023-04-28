46937ca6d558603077ce779b2062516c89f493d7
Imabanana80 <imabanana80.hallo@gmail.com>
2023-04-28 23:32:35 +0800

    fixed rare bug with weird domains

--- src/commands/utility/whoislookup.js
+++ src/commands/utility/whoislookup.js
@@ -10,7 +10,9 @@ module.exports = class whoislookupSlashCommand extends baseSlashCommand {
     await interaction.deferReply();
     const domain = interaction.options.get("domain").value;
     var domaindata = await whoiser.domain(`${domain}`);
-    delete domaindata["whois.verisign-grs.com"].text;
+    if (domaindata["whois.verisign-grs.com"].text) {
+      delete domaindata["whois.verisign-grs.com"].text;
+    }
     var buff = Buffer.from(JSON.stringify(domaindata, null, " "), "utf-8");
     await interaction.editReply({
       files: [{ attachment: buff, name: "domaindata.txt" }],
